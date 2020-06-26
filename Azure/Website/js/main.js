7ea6b21dd978356830d8e44cc3faf939365ad5c7
Bert Wagner <github@bertwagner.com>
2020-06-25 22:02:51 -0400

    adds static website with chart.js

---
+++ Azure/Website/js/main.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById('gauge').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
+//fetch this  https://gardengauge.azurewebsites.net/api/ReadSensor5min?start_date=2020-06-24&start_hour=8&stop_date=2020-06-28&stop_hour=10
